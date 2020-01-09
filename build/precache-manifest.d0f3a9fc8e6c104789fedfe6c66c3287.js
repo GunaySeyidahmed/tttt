c5a498d31f1d7db96903de1cdaf6da06c1fe0fcd
Gunay Mammadli <gunaymammadli@Gunays-MacBook-Pro.local>
2020-01-09 11:50:52 +0400

    first

---
+++ build/precache-manifest.d0f3a9fc8e6c104789fedfe6c66c3287.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d1279c7365edee33129bf56cc4abcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f698a5b8e78f4259ebaa",
+    "url": "/static/css/2.d8c2d496.chunk.css"
+  },
+  {
+    "revision": "7e6cb80e5f843d1b5628",
+    "url": "/static/css/main.146a1a14.chunk.css"
+  },
+  {
+    "revision": "f698a5b8e78f4259ebaa",
+    "url": "/static/js/2.bf9c7eff.chunk.js"
+  },
+  {
+    "revision": "7e6cb80e5f843d1b5628",
+    "url": "/static/js/main.3ab9f7b2.chunk.js"
+  },
+  {
+    "revision": "e5453e80391abb3be853",
+    "url": "/static/js/runtime-main.0cabf068.js"
+  },
+  {
+    "revision": "8013ed069f33c6cc7d5d8b6ae2411553",
+    "url": "/static/media/11″ iPad Pro.8013ed06.jpg"
+  },
+  {
+    "revision": "01287d2ed5fe506e793ea1437ecd05a7",
+    "url": "/static/media/AngleciaProDisplay.01287d2e.woff"
+  },
+  {
+    "revision": "628abee13d4d88cd9b0dd8f055998203",
+    "url": "/static/media/AngleciaProDisplay.628abee1.svg"
+  },
+  {
+    "revision": "c39dd64878c3e53f1c74ef1647a2b646",
+    "url": "/static/media/AngleciaProDisplay.c39dd648.eot"
+  },
+  {
+    "revision": "f78e3033dbfe8ba85ca38e33f63aacbb",
+    "url": "/static/media/AngleciaProDisplay.f78e3033.woff2"
+  },
+  {
+    "revision": "fb1d1885fd0697e7fc06da0ee93e1e81",
+    "url": "/static/media/AngleciaProDisplay.fb1d1885.ttf"
+  },
+  {
+    "revision": "dd5bdcd8941f881f961c5f01b62098b7",
+    "url": "/static/media/Contact-img.dd5bdcd8.png"
+  },
+  {
+    "revision": "480ecf1f8e9c1ab793ff7afc01ece4e1",
+    "url": "/static/media/Fuad Ibrahimov.480ecf1f.jpg"
+  },
+  {
+    "revision": "0a388c3670f6d887112549b3f5627336",
+    "url": "/static/media/Fuad-Ibrahimov.0a388c36.png"
+  },
+  {
+    "revision": "5ac12814b60dcff62dc123a58102be53",
+    "url": "/static/media/Fuad-Ibrahimov.5ac12814.jpg"
+  },
+  {
+    "revision": "ec13ff8986c5c2da2c78210a0fc407b5",
+    "url": "/static/media/Fuad-Ibrahimov.ec13ff89.png"
+  },
+  {
+    "revision": "f33dd88f8b1c9bce660c589d5a50ad14",
+    "url": "/static/media/IMSLP77147-PMLP80636-Daphnis_and_Chloe_-_Ravel.f33dd88f.mp3"
+  },
+  {
+    "revision": "b6b13d650a99779ea47e281bee60a0dd",
+    "url": "/static/media/Media-1.b6b13d65.jpg"
+  },
+  {
+    "revision": "12dd57366fa2fbd2e51d78bf6e9d3679",
+    "url": "/static/media/Media-3.12dd5736.jpg"
+  },
+  {
+    "revision": "2b8243c3f99dd82d9d764fb3c6589d44",
+    "url": "/static/media/Montserrat-Bold.2b8243c3.eot"
+  },
+  {
+    "revision": "3cb5de6e3f2b20560aab6183126d5880",
+    "url": "/static/media/Montserrat-Bold.3cb5de6e.ttf"
+  },
+  {
+    "revision": "3d778a5c0eadba61c24f3cf00f3cc37f",
+    "url": "/static/media/Montserrat-Bold.3d778a5c.woff"
+  },
+  {
+    "revision": "55937962060bf207b550e5542f2d5d03",
+    "url": "/static/media/Montserrat-Bold.55937962.svg"
+  },
+  {
+    "revision": "6c8ff44b950abeec63a73285cf87aa5e",
+    "url": "/static/media/Montserrat-Bold.6c8ff44b.woff2"
+  },
+  {
+    "revision": "0048f40ce120adf84267a41bad3e7a4c",
+    "url": "/static/media/Montserrat-Regular.0048f40c.eot"
+  },
+  {
+    "revision": "02c4bf4d86d0bec6eed1520b4334abef",
+    "url": "/static/media/Montserrat-Regular.02c4bf4d.svg"
+  },
+  {
+    "revision": "5eefdffd5b9dade816a088daa446986a",
+    "url": "/static/media/Montserrat-Regular.5eefdffd.woff"
+  },
+  {
+    "revision": "9121c084c24c7b8023ffdb29d9dc5e86",
+    "url": "/static/media/Montserrat-Regular.9121c084.ttf"
+  },
+  {
+    "revision": "f604e095817e215d604f9abf01ad2029",
+    "url": "/static/media/Montserrat-Regular.f604e095.woff2"
+  },
+  {
+    "revision": "04f325a33f5f76c7d8706ac79bf0130e",
+    "url": "/static/media/StephanieMarieJF.04f325a3.woff2"
+  },
+  {
+    "revision": "32ef39952bc8483c433dddce93892895",
+    "url": "/static/media/StephanieMarieJF.32ef3995.eot"
+  },
+  {
+    "revision": "8f77a21b729ae30c4aa7c06c2185b1a5",
+    "url": "/static/media/StephanieMarieJF.8f77a21b.svg"
+  },
+  {
+    "revision": "c121371c10d5c3b612f8f74e563c86d1",
+    "url": "/static/media/StephanieMarieJF.c121371c.ttf"
+  },
+  {
+    "revision": "feebed11e865cce84e90df97013ab69c",
+    "url": "/static/media/StephanieMarieJF.feebed11.woff"
+  },
+  {
+    "revision": "971243b5a44c05afd150eeaf118e2efd",
+    "url": "/static/media/bbc.971243b5.png"
+  },
+  {
+    "revision": "058c63711a8ed02efad6c5eaa1b3216d",
+    "url": "/static/media/biography.058c6371.jpg"
+  },
+  {
+    "revision": "2bc72b04a2872c48590380f3a9d81302",
+    "url": "/static/media/biography_img.2bc72b04.jpg"
+  },
+  {
+    "revision": "a6940449e7e969a839d7b6e613438976",
+    "url": "/static/media/concert1.a6940449.jpg"
+  },
+  {
+    "revision": "c6f239df5a1a17cb9889a59170740032",
+    "url": "/static/media/concert2.c6f239df.jpg"
+  },
+  {
+    "revision": "a9ed85da55c64748bf5d130e07ca9a7d",
+    "url": "/static/media/concert3.a9ed85da.jpg"
+  },
+  {
+    "revision": "c6f239df5a1a17cb9889a59170740032",
+    "url": "/static/media/concert4.c6f239df.jpg"
+  },
+  {
+    "revision": "34b8b39662cda8b7da129fa74af90386",
+    "url": "/static/media/concert5.34b8b396.jpg"
+  },
+  {
+    "revision": "a9ed85da55c64748bf5d130e07ca9a7d",
+    "url": "/static/media/concert6.a9ed85da.jpg"
+  },
+  {
+    "revision": "c6f239df5a1a17cb9889a59170740032",
+    "url": "/static/media/concert7.c6f239df.jpg"
+  },
+  {
+    "revision": "538676cc1ac28948959bdcf9b245ab2e",
+    "url": "/static/media/concerts.538676cc.jpg"
+  },
+  {
+    "revision": "ecca0491491535896154f095a0fe22f2",
+    "url": "/static/media/contact.ecca0491.jpg"
+  },
+  {
+    "revision": "aa205f9e6bac34fbe733803d089ad55c",
+    "url": "/static/media/down_icon.aa205f9e.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "13bce0e91dcf19fce167147748d3df41",
+    "url": "/static/media/iPhone XR.13bce0e9.jpg"
+  },
+  {
+    "revision": "c174638cd9bdf2674db19ffad671b4d6",
+    "url": "/static/media/image.c174638c.png"
+  },
+  {
+    "revision": "df7a9de3454bdad896da2e05a0b9d58e",
+    "url": "/static/media/mainpage.df7a9de3.jpg"
+  },
+  {
+    "revision": "866a669afb1222bf358f553e129bb7f5",
+    "url": "/static/media/media.866a669a.jpg"
+  },
+  {
+    "revision": "f67601dbbce7b6e4e6050a320a6c3173",
+    "url": "/static/media/news.f67601db.png"
+  },
+  {
+    "revision": "f377e75a1c96c1509b22672dd7ee6252",
+    "url": "/static/media/press.f377e75a.jpg"
+  },
+  {
+    "revision": "084b19a2813442fa2f958a2e2a15a5c1",
+    "url": "/static/media/press1.084b19a2.jpg"
+  },
+  {
+    "revision": "4333c1486433b82721bfb96d31bd2584",
+    "url": "/static/media/press2.4333c148.jpg"
+  },
+  {
+    "revision": "dc8adc7bbbb258f3fa37603d08301e92",
+    "url": "/static/media/press3.dc8adc7b.jpg"
+  },
+  {
+    "revision": "efcdf6c4c59af456a067e6968129c58d",
+    "url": "/static/media/press4.efcdf6c4.jpg"
+  },
+  {
+    "revision": "084b19a2813442fa2f958a2e2a15a5c1",
+    "url": "/static/media/press5.084b19a2.jpg"
+  },
+  {
+    "revision": "4333c1486433b82721bfb96d31bd2584",
+    "url": "/static/media/press6.4333c148.jpg"
+  },
+  {
+    "revision": "dc8adc7bbbb258f3fa37603d08301e92",
+    "url": "/static/media/press7.dc8adc7b.jpg"
+  },
+  {
+    "revision": "efcdf6c4c59af456a067e6968129c58d",
+    "url": "/static/media/press8.efcdf6c4.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "00e6cef644b459e4450ca4100f9f5da4",
+    "url": "/static/media/timeline.00e6cef6.jpg"
+  },
+  {
+    "revision": "41bdd74c4450c2094477395002d7a6dd",
+    "url": "/static/media/times.41bdd74c.png"
+  },
+  {
+    "revision": "44934644cb252af7896f3baf06ffc791",
+    "url": "/static/media/yeni-cag.44934644.png"
+  },
+  {
+    "revision": "1814204a4498ef91528518ef2a991acb",
+    "url": "/static/media/youtube_icon.1814204a.svg"
+  }
+]);
